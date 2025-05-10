9548e5f5b798b06aabb815cc6e6ee160228a8864
Student USO VM User <student@stud.acs.upb.ro>
2025-05-11 00:24:30 +0300

    lcd and keyboard connected v2

--- src/main.cpp
+++ src/main.cpp
@@ -34,9 +34,7 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
   char key = keypad.getKey();
-  // lcd.clear();
   
   if (key) {
     // Serial.println(key);
